73c6c35e2f200324c50174f4dcf8b9308b8ffc16
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2015-05-09 12:08:04 +0200

    Moved HYDRO to /HPC_HYDRO/.

--- HYDRO/HYDRO/C/MonoRef/Src/constoprim.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTOPRIM_H_INCLUDED
-#define CONSTOPRIM_H_INCLUDED
-
-#include "utils.h"
-
-void constoprim(double *RESTRICT u, double *RESTRICT q, double *RESTRICT e,
-                const long n, const long Hnxyt, const long Hnvar, const double Hsmallr);
-
-#endif // CONSTOPRIM_H_INCLUDED
