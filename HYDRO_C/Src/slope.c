33adcc548892d4cc9bcb94c20f6b3469fcab74d7
Rafael Kueng <rafi.kueng@gmx.ch>
2015-05-14 21:50:26 +0200

    prettifying code using kdev

--- HYDRO_C/Src/slope.c
+++ HYDRO_C/Src/slope.c
@@ -21,34 +21,33 @@
 // #endif
 
 void
-slope(double *RESTRICT q, double *RESTRICT dq, const long narray,
-      const long Hnvar, const long Hnxyt, const double slope_type)
-{
+slope ( double *RESTRICT q, double *RESTRICT dq, const long narray,
+        const long Hnvar, const long Hnxyt, const double slope_type ) {
     long n, i, ijmin, ijmax;
 #define IHVW(i, v) ((i) + (v) * Hnxyt)
 
-    WHERE("slope");
+    WHERE ( "slope" );
     ijmin = 0;
     ijmax = narray;
 
-    for (n = 0; n < Hnvar; n++) {
-        for (i = ijmin + 1; i < ijmax - 1; i++) {
-	  double dlft, drgt, dcen, dsgn, slop, dlim;
-	  long ihvwin, ihvwimn, ihvwipn;
-	  ihvwin = IHVW(i, n);
-	  ihvwimn = IHVW(i - 1, n);
-	  ihvwipn = IHVW(i + 1, n);
-	  dlft = slope_type * (q[ihvwin] - q[ihvwimn]);
-	  drgt = slope_type * (q[ihvwipn] - q[ihvwin]);
-	  dcen = half * (dlft + drgt) / slope_type;
-	  dsgn = (dcen > 0) ? (double) 1.0 : (double) -1.0;   // sign(one, dcen);
-	  slop = DABS(drgt);
-	  slop = (double) MIN(DABS(dlft), slop);
-	  dlim = slop;
-	  if ((dlft * drgt) <= zero) {
-	    dlim = zero;
-	  }
-	  dq[ihvwin] = dsgn * (double) MIN(dlim, DABS(dcen));
+    for ( n = 0; n < Hnvar; n++ ) {
+        for ( i = ijmin + 1; i < ijmax - 1; i++ ) {
+            double dlft, drgt, dcen, dsgn, slop, dlim;
+            long ihvwin, ihvwimn, ihvwipn;
+            ihvwin = IHVW ( i, n );
+            ihvwimn = IHVW ( i - 1, n );
+            ihvwipn = IHVW ( i + 1, n );
+            dlft = slope_type * ( q[ihvwin] - q[ihvwimn] );
+            drgt = slope_type * ( q[ihvwipn] - q[ihvwin] );
+            dcen = half * ( dlft + drgt ) / slope_type;
+            dsgn = ( dcen > 0 ) ? ( double ) 1.0 : ( double ) -1.0; // sign(one, dcen);
+            slop = DABS ( drgt );
+            slop = ( double ) MIN ( DABS ( dlft ), slop );
+            dlim = slop;
+            if ( ( dlft * drgt ) <= zero ) {
+                dlim = zero;
+            }
+            dq[ihvwin] = dsgn * ( double ) MIN ( dlim, DABS ( dcen ) );
         }
     }
 }                               // slope
