2dbfa17dfaf4a5bb83a6d034ef5c0135dc1daf2a
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2015-05-16 23:31:59 +0200

    Merged my stuff.

--- HYDRO_C/Src/make_boundary.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAKE_BOUNDARY_H_INCLUDED
-#define MAKE_BOUNDARY_H_INCLUDED
-
-void make_boundary(long idim, const hydroparam_t H, hydrovar_t * Hv);
-void MPI_get_boundary_start(long idim, const hydroparam_t H, hydrovar_t * Hv);
-void MPI_get_boundary_end(long idim, const hydroparam_t H, hydrovar_t * Hv);
-void MPI_get_boundary(long idim, const hydroparam_t H, hydrovar_t * Hv);
-
-#endif // MAKE_BOUNDARY_H_INCLUDED
