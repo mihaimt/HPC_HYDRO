d4904ffc0f7a0d7db7d46c76f6416251ec7a1c84
Rafael Kueng <rafi.kueng@gmx.ch>
2015-05-25 19:52:04 +0200

    ALTERNATIVE filenaming

--- HYDRO_C/Src/vtkfile.c
+++ HYDRO_C/Src/vtkfile.c
@@ -21,7 +21,7 @@ vtkfile(long step, const hydroparam_t H, hydrovar_t * Hv)
     long i, j, nv;
 
     WHERE("vtkfile");
-    sprintf(name, "outputvtk_%05ld.vts", step);
+    sprintf(name, "outputvtk_%03i_%06i.vts", H.rank, step);
     fic = fopen(name, "w");
     if (fic == NULL) {
         fprintf(stderr, "Ouverture du fichier %s impossible\n", name);
